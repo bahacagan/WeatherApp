0bb9b5c585d1c197ef94a28d8b1aced9a30217a1
bahacagan <bahacagan@gmail.com>
2025-09-09 17:30:26 +0300

    [Add] some networking files

---
+++ WeatherApp/Networking/ForecastService.swift
@@ -0,0 +1,8 @@
+//
+//  ForecastService.swift
+//  WeatherApp
+//
+//  Created by Çağan  on 9.09.2025.
+//
+
+import Foundation
---
+++ WeatherApp/Networking/GeocodingService.swift
@@ -0,0 +1,9 @@
+//
+//  GeocodingService.swift
+//  WeatherApp
+//
+//  Created by Çağan  on 9.09.2025.
+//
+
+import Foundation
+
