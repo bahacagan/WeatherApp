403e686b532bc24a9c660814906e7d03615e5db2
bahacagan <bahacagan@gmail.com>
2025-09-10 10:29:31 +0300

    Created the ForecastResponse models

--- WeatherApp/Model/ForecastResponse.swift
+++ WeatherApp/Model/ForecastResponse.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+
+struct ForecastResponse: Decodable {
+    let latitude: Double
+    let longitude: Double
+    let timezone: String
+    let current_weather: CurrentWeather?
+    let hourly: HourlyForecast?
+    let daily: DailyForecast?
+}
+
+
+struct CurrentWeather: Decodable {
+    let temperature: Double
+    let windspeed: Double
+    let winddirection: Double
+    let weathercode: Int
+    let time: String
+}
+
+struct HourlyForecast: Decodable {
+    let time: [String]
+    let temperature_2m: [Double]?
+}
+
+struct DailyForecast: Decodable {
+    let time: [String]
+    let temperature_2m_max: [Double]?
+    let temperature_2m_min: [Double]?
+}
+
