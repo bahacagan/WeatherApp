c1ebd0a99c58aaafb18ec302aeb8a3f6fe5c28f5
bahacagan <bahacagan@gmail.com>
2025-09-10 09:50:58 +0300

    Added some necessary files (model and networking)
    * [Add] some networking files
    
    * Added the model files

---
+++ WeatherApp/Model/GeocodingResponse.swift
@@ -0,0 +1,8 @@
+//
+//  GeocodingResponse.swift
+//  WeatherApp
+//
+//  Created by Çağan  on 10.09.2025.
+//
+
+import Foundation
---
+++ WeatherApp/Networking/Endpoints.swift
@@ -0,0 +1,19 @@
+//
+//  Endpoints.swift
+//  WeatherApp
+//
+//  Created by Çağan  on 9.09.2025.
+//
+
+import Foundation
+
+
+struct API {
+    struct Geocoding {
+        static let baseURL = "https://geocoding-api.open-meteo.com/v1"
+    }
+
+    struct Forecast {
+        static let baseURL = "https://api.open-meteo.com/v1"
+    }
+}
