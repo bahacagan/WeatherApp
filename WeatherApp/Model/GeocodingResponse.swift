e3c78fc25cf6e7b269ff940576eaaa9db5c66f3b
bahacagan <bahacagan@gmail.com>
2025-09-10 10:18:24 +0300

    Created the GeocodingResponse model

--- WeatherApp/Model/GeocodingResponse.swift
+++ WeatherApp/Model/GeocodingResponse.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct GeocodingResponse: Decodable {
+    let results: [GeocodingResult]?
+}
+
+
+struct GeocodingResult: Decodable, Identifiable {
+    let id: Int
+    let name: String
+    let latitude: Double
+    let longitude: Double
+    let country: String?
+    let admin1: String?
+    let timezone: String
+    let population: Int?
+}
+
+
