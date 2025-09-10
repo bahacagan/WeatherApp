6b9d68bfa23606d7c098ba419344f35751149b2b
bahacagan <bahacagan@gmail.com>
2025-09-10 10:58:02 +0300

    Created the Geocoding service

--- WeatherApp/Networking/GeocodingService.swift
+++ WeatherApp/Networking/GeocodingService.swift
@@ -7,3 +7,37 @@
 
 import Foundation
 
+
+final class GeocodingService {
+    private let decoder: JSONDecoder
+    init() {
+        decoder = JSONDecoder()
+    }
+    
+    func fetchCoordinates(for city: String) async throws -> GeocodingResponse {
+        var components = URLComponents()
+        components.scheme = API.Geocoding.scheme
+        components.host = API.Geocoding.host
+        components.path = API.Geocoding.path
+        components.queryItems = [
+            URLQueryItem(name: "name", value: city),
+            URLQueryItem(name: "count", value: "1"),
+            URLQueryItem(name: "language", value: "tr")
+        ]
+        
+        guard let url = components.url else {
+            throw URLError(.badURL)
+        }
+        
+        let (data,response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+                200..<300 ~= httpResponse.statusCode
+        else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return try decoder.decode(GeocodingResponse.self, from: data)
+        
+    }
+}
