66195522514dbac17ac13967c7fb53a47effae2e
bahacagan <bahacagan@gmail.com>
2025-09-10 16:19:57 +0300

    Core app is done

---
+++ WeatherApp/View/WeatherDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherDetailView.swift
+//  WeatherApp
+//
+//  Created by Çağan  on 10.09.2025.
+//
+
+import SwiftUI
+
+struct WeatherDetailView: View {
+    let GeocodingResult: GeocodingResult
+    var body: some View {
+    }
+}
+
+#Preview {
+    WeatherDetailView()
+}
